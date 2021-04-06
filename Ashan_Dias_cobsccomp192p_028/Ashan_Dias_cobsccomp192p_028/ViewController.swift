93f8675bd8b091eb07514d3ad12f3855602c64fb
AshanDias <ashandias.info@gmail.com>
2021-04-06 15:25:54 +0530

    Initial

---
+++ Ashan_Dias_cobsccomp192p_028/Ashan_Dias_cobsccomp192p_028/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ashan_Dias_cobsccomp192p_028
+//
+//  Created by Ashan Dias on 2021-04-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Ashan_Dias_cobsccomp192p_028/Ashan_Dias_cobsccomp192p_028Tests/Ashan_Dias_cobsccomp192p_028Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Ashan_Dias_cobsccomp192p_028Tests.swift
+//  Ashan_Dias_cobsccomp192p_028Tests
+//
+//  Created by Ashan Dias on 2021-04-06.
+//
+
+import XCTest
+@testable import Ashan_Dias_cobsccomp192p_028
+
+class Ashan_Dias_cobsccomp192p_028Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
