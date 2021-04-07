8d7def7cfbd57e01374a0b0c025f9cabe4236da2
AshanDias <ashandias.info@gmail.com>
2021-04-07 13:46:32 +0530

    Added Locatinon allow scx

---
+++ Ashan_Dias_cobsccomp192p_028/Ashan_Dias_cobsccomp192p_028/Controllers/LocationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LocationViewController.swift
+//  Ashan_Dias_cobsccomp192p_028
+//
+//  Created by Ashan Dias on 2021-04-07.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
