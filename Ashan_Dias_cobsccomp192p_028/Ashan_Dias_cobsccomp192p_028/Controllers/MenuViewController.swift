d9db071d48bb817904b1bea20b786ddb351b0290
AshanDias <ashandias.info@gmail.com>
2021-04-08 14:17:29 +0530

    Added Tab bar nav controller

---
+++ Ashan_Dias_cobsccomp192p_028/Ashan_Dias_cobsccomp192p_028/Controllers/MenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MenuViewController.swift
+//  Ashan_Dias_cobsccomp192p_028
+//
+//  Created by Ashan Dias on 2021-04-08.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+  
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
