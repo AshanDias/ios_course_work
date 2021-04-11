c04e48c4f9a75cefa8e85a1f8bc757d46324a2c2
AshanDias <ashandias.info@gmail.com>
2021-04-11 18:54:10 +0530

    Added Menu Item to firebase

--- Ashan_Dias_cobsccomp192p_028/Ashan_Dias_cobsccomp192p_028/Controllers/MenuViewController.swift
+++ Ashan_Dias_cobsccomp192p_028/Ashan_Dias_cobsccomp192p_028/Controllers/MenuViewController.swift
@@ -6,13 +6,26 @@
 //
 
 import UIKit
-
+import iOSDropDown
+import Firebase
 class MenuViewController: UIViewController {
-
+    
+    private let database = Database.database().reference()
+    
     @IBOutlet weak var button:UIButton!
+    @IBOutlet weak var dropDown : DropDown!
+    @IBOutlet weak var name :UITextField!
+    @IBOutlet weak var desc :UITextField!
+    @IBOutlet weak var price :UITextField!
+    @IBOutlet weak var discount :UITextField!
+    @IBOutlet weak var sellType :UISwitch!
+    var catSelectedItem = ""
+    var imageId = ""
     override func viewDidLoad() {
         super.viewDidLoad()
+        button.isEnabled=false
         setBtn()
+        loadDropDown()
         // Do any additional setup after loading the view.
     }
     
@@ -22,7 +35,41 @@ class MenuViewController: UIViewController {
         button.layer.borderWidth = 0
         
     }
-
+    
+    func loadDropDown(){
+        let group = DispatchGroup()
+        self.database.child("Category").getData { (error, snapshot) in
+            if snapshot.exists(){
+                let dataChange = snapshot.value as! [String:AnyObject]
+                group.wait()
+               
+                dataChange.forEach({ (key,val) in
+                  
+                    
+                    self.dropDown.optionArray.append(val as! String)
+                    
+                  
+                })
+                
+                group.notify(queue: .main) {
+                        // do something here when loop finished
+                    self.button.isEnabled=true
+                    self.dropDown.didSelect{(selectedText , index ,id) in
+                        self.catSelectedItem=selectedText
+                    }
+                }
+            }
+        }
+    }
+     
+    @IBAction func btnAdd(_ sender: Any) {
+       
+        
+        let menu=MenuItem(name: name.text!, desc: desc.text!, price: Double(price.text!)!, img: imageId, category: catSelectedItem, discount: Int(discount.text!)!, sellType: sellType.isOn)
+        
+            let child = Int.random(in: 0...1000)
+        self.database.child("MenuItems").child(String(child)).setValue(menu.getJSON())
+    }
     /*
     // MARK: - Navigation
 
---
+++ Ashan_Dias_cobsccomp192p_028/Model/MenuItem.swift
@@ -0,0 +1,30 @@
+//
+//  MenuItem.swift
+//  Ashan_Dias_cobsccomp192p_028
+//
+//  Created by Ashan Dias on 2021-04-11.
+//
+
+import Foundation
+
+struct MenuItem{
+    var name:String
+    var desc:String
+    var price:Double
+    var img:String
+    var category:String
+    var discount:Int
+    var sellType:Bool
+    
+    func getJSON() -> NSMutableDictionary {
+           let dict = NSMutableDictionary()
+           dict.setValue(name, forKey: "name")
+            dict.setValue(desc, forKey: "desc")
+            dict.setValue(price, forKey: "price")
+            dict.setValue(img, forKey: "img")
+            dict.setValue(category, forKey: "category")
+            dict.setValue(discount, forKey: "discount")
+            dict.setValue(sellType, forKey: "sellTypes")
+           return dict
+       }
+}
