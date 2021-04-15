f24114af6d355c25f50b9705f51599394c7ccfe4
AshanDias <ashandias.info@gmail.com>
2021-04-15 19:06:14 +0530

    Added print view data filter

--- Ashan_Dias_cobsccomp192p_028/Ashan_Dias_cobsccomp192p_028/Controllers/PrintDataViewController.swift
+++ Ashan_Dias_cobsccomp192p_028/Ashan_Dias_cobsccomp192p_028/Controllers/PrintDataViewController.swift
@@ -11,10 +11,11 @@ import Firebase
 class PrintDataViewController: UIViewController {
 
     private let database = Database.database().reference()
-    
+    var refreshControl: UIRefreshControl?
     var ordersItems: [OrderDetails] = [
       
     ]
+    var result: [OrderDetails] = []
     @IBOutlet weak var tbl_print:UITableView!
     
     @IBOutlet weak var dp1: UIDatePicker!
@@ -27,11 +28,15 @@ class PrintDataViewController: UIViewController {
         tbl_print.register(nib, forCellReuseIdentifier: "PrintTableViewCell")
         tbl_print.delegate=self
         tbl_print.dataSource=self
+        loadData()
         // Do any additional setup after loading the view.
     }
     
+    
+    
+    
     func loadData(){
-        
+        ordersItems.removeAll()
         let group = DispatchGroup()
         self.database.child("OrderItems").getData { (error, snapshot) in
             if snapshot.exists() {
@@ -61,21 +66,46 @@ class PrintDataViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        loadData()
+//        loadData()
     }
     
     @IBAction func btnProcess(_ sender: Any) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/YY"
+        var d1=dateFormatter.string(from: dp1.date)
+        if(ordersItems.count==0 && ordersItems.count==0){
+            loadData()
+            dp1.date=Date()
+        }
+        result.removeAll()
+        for itmes in ordersItems{
+            
+         
+            let substring = d1.dropFirst(1)
+            let realString1 = String(substring)
+            var date=String(itmes.date!)
+            
+            if(date == realString1){
+                result.append(itmes)
+                }
+           
+           
+        }
+
+        ordersItems.removeAll()
+       
+        
+        for data in result {
+            ordersItems.append(data)
+        }
+        
+        
+        tbl_print.reloadData()
+        
+       
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
 
@@ -84,6 +114,21 @@ extension PrintDataViewController:UITableViewDelegate,UITableViewDataSource{
     
    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return ordersItems.count
+//        if ordersItems.count == 0 {
+//            let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+//                   messageLabel.text = "No records found !"
+//                   messageLabel.textColor = .black
+//                   messageLabel.numberOfLines = 0
+//                   messageLabel.textAlignment = .center
+//                   messageLabel.font = UIFont(name: "TrebuchetMS", size: 15)
+//                   messageLabel.sizeToFit()
+//
+//            tbl_print.backgroundView = messageLabel
+//        }else{
+//            tbl_print.backgroundView = nil
+//        }
+//
         return ordersItems.count
     }
     
