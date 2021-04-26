dde790b195cedd5515e4eb616e9f3dddc447c532
AshanDias <ashandias.info@gmail.com>
2021-04-26 10:18:54 +0530

    Added forget password

--- Ashan_Dias_cobsccomp192p_028/Ashan_Dias_cobsccomp192p_028/Controllers/ForgetPasswordViewController.swift
+++ Ashan_Dias_cobsccomp192p_028/Ashan_Dias_cobsccomp192p_028/Controllers/ForgetPasswordViewController.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import Firebase
 
 class ForgetPasswordViewController: UIViewController {
     @IBOutlet weak var button:UIButton!
+    @IBOutlet weak var txt_email: UITextField!
+    let auth = Auth.auth()
     override func viewDidLoad() {
         super.viewDidLoad()
         setBtn()
@@ -23,9 +26,57 @@ class ForgetPasswordViewController: UIViewController {
     }
     
     @IBAction func Proceed(_ sender: Any) {
-        performSegue(withIdentifier: "preceedFPWD", sender: nil)
+        if isEmpty([txt_email]){
+            displayAlert(title: "Field Empty", message: "You should at least provide your email address for forget password")
+        }else{
+            auth.sendPasswordReset(withEmail: txt_email.text!, completion: {error in
+                if error == nil{
+                    self.displayAlert(title:"Message",message: "A Email is successfully submited to \(self.txt_email.text ?? "undefined")")
+                }else{
+                    self.alertEmailValidationErrorIfPresent(err: error!)
+                }
+            })
+        }
+//        performSegue(withIdentifier: "preceedFPWD", sender: nil)
     }
     
+    @IBAction func btnCancel(_ sender: Any) {
+        performSegue(withIdentifier: "cancelForgetpwd", sender: self)
+    }
+    
+    func displayAlert(title:String,message:String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+   
+    
+    func alertEmailValidationErrorIfPresent(err:Error){
+        var message:String?
+        switch AuthErrorCode(rawValue: err._code) {
+        case .emailAlreadyInUse:
+            message = "email is already in use.Please use another email"
+        case.invalidEmail,.invalidRecipientEmail:
+            message = "Invalid email address.Please enter a valid one"
+        default:
+            message = nil
+        }
+        if(message != nil){
+            displayAlert(title: "Email field error", message: message!)
+        }
+    }
+
+    
+    func isEmpty(_ fields:[UITextField]) -> Bool {
+        //utility function is check if the field is empty
+        for field in fields{
+            if field.text == nil || field.text!.count == 0{
+                return true
+            }
+        }
+        return false
+    }
     /*
     // MARK: - Navigation
 
